c6a1689126e767dd46e4af1539e7e4d240657a2f
aprileelcich <aprile.elcich@gmail.com>
2019-05-01 14:20:24 +0200

    update website

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-var http = require('http');
-var send = require('send');
-var url = require('url');
+var http = require('http')
+var send = require('send')
+var url = require('url')
 
-var app = http.createServer(function(req, res){
-
+var app = http.createServer(function (req, res) {
   var p = url.parse(req.url).pathname
   send(req, p, { root: __dirname })
-  .pipe(res);
-
-}).listen(8080);
-
+    .pipe(res)
+}).listen(8080)
